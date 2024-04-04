f02a6e38a20bae61441cd78848da2b2991be13ab
Marcelo Mariano <marcelomariano11@gmail.com>
2024-04-03 22:20:27 -0400

    UserButton adjust3

--- app/(client)/users/page.tsx
+++ app/(client)/users/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import UserButton from "../../components/UserButton.jsx";
+import UserButton from "../../components/UserButton";
 
 interface User {
   id: number;
